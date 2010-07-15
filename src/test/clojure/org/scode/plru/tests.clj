b5a6c5d13c87fd72ad84112e8975515b6816f564
Peter Schuller <peter.schuller@infidyne.com>
2010-07-15 23:31:46 +0200

    Improve unit tests.

--- src/test/clojure/org/scode/plru/tests.clj
+++ src/test/clojure/org/scode/plru/tests.clj
@@ -8,26 +8,26 @@
 
 (deftest get-non-existing
   (let [[val _] (plru/lru-get (plru/make-lru 2) "key")]
-    (is val nil)))
+    (is (= nil val) "empty lru should give nil back")))
 
 (deftest get-default
   (let [[val _] (plru/lru-get (plru/make-lru 2) "key" "defaultval")]
-    (is val "defaultval")))
+    (is (= "defaultval" val) "default value should be given on non-existent key")))
 
 (deftest get-existing
   (let [c (plru/lru-put (plru/make-lru 10) "key" "value")
         [val _] (plru/lru-get c "key")]
-    (is val "value")))
+    (is (="value" val) "should get existing value assocaited with key")))
 
 (deftest eviction-of-unused
   (let [c (plru/make-lru 2)
         c1 (plru/lru-put c "key1" "val1")
         c2 (plru/lru-put c1 "key2" "val2")
         [val _] (plru/lru-get c2 "key1")]
-    (is val "val1")
+    (is (= "val1" val) "key1 should not yet be evicted")
     (let [c3 (plru/lru-put c2 "key3" "val3")
           [val _] (plru/lru-get c3 "key1")]
-      (is val nil))))
+      (is (= nil val) "key1 should now be evicted"))))
 
 (run-tests)
 
