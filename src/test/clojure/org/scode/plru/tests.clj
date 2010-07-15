b759793ff9397155f72cf3741b67fa687a7c1c62
Peter Schuller <peter.schuller@infidyne.com>
2010-07-15 23:41:25 +0200

    Add peak tests.

--- src/test/clojure/org/scode/plru/tests.clj
+++ src/test/clojure/org/scode/plru/tests.clj
@@ -10,14 +10,25 @@
   (let [[val _] (plru/lru-get (plru/make-lru 2) "key")]
     (is (= nil val) "empty lru should give nil back")))
 
+(deftest peak-non-existing
+  (is (= nil (plru/lru-peak (plru/make-lru 2) "key")) "empty lru should peak nil"))
+
 (deftest get-default
   (let [[val _] (plru/lru-get (plru/make-lru 2) "key" "defaultval")]
     (is (= "defaultval" val) "default value should be given on non-existent key")))
 
+(deftest peak-default
+  (is (= "default" (plru/lru-peak (plru/make-lru 2) "key" "default"))
+      "empty lru should peak default value"))
+
 (deftest get-existing
   (let [c (plru/lru-put (plru/make-lru 10) "key" "value")
         [val _] (plru/lru-get c "key")]
-    (is (="value" val) "should get existing value assocaited with key")))
+    (is (= "value" val) "should get existing value assocaited with key")))
+
+(deftest peak-existing
+  (let [c (plru/lru-put (plru/make-lru 10) "key" "value")]
+    (is (= "value" (plru/lru-peak c "key")))))
 
 (deftest eviction-of-unused
   (let [c (plru/make-lru 2)
